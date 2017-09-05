ebd6e2bb445578d55c856c15f9c062a4ea315f67
Zomaru-kun <renaldypurwanto@gmail.com>
2017-09-05 08:32:42 +0700

    Ditinggal rabi

--- Change_bootanim.sh
+++ Change_bootanim.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 # Script pengganti boot animation secara otomatis
 # Author : Renaldy P <zomaru@github.com>
-# Pastikan simpan file boot animation di penyimpanan internal
+# Pastikan simpan file boot animation ada di penyimpanan internal
 #
 # This software is licensed under the terms of the GNU General Public
 # License version 2, as published by the Free Software Foundation, and
