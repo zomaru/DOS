bcaeea1a98805c840b0c23414f0bf20ccc46bde4
Zomaru-kun <renaldypurwanto@gmail.com>
2018-05-01 22:01:31 +0700

    Belajar bahasa Ruby

---
+++ test.rb
@@ -0,0 +1,12 @@
+print "Siapa namamu?"
+name = gets.chomp
+name.capitalize!
+
+print "Kamu lagi ngapain? "
+whazzup = gets.chomp
+whazzup.capitalize!
+
+print "Umurmu berapa?"
+age = gets.chomp
+
+puts "Namamu adalah, #{name}, kamu lagi #{whazzup}, dan umurmu #{age}"
